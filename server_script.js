aea867bae6500f82afd18a452704d424d6a298f7
aswmac <aswmac@yahoo.com>
2025-05-19 14:03:59 -0500

    Update server_script.js
    Attempting to add the websockets functionality to be able to serve the changes of the svg to be viewable by the user

--- server_script.js
+++ server_script.js
@@ -3,13 +3,28 @@
 
 const express = require('express');
 const http = require('http');
+const WebSocket = require('ws');
 const socketIo = require('socket.io');
+const { diffChars } = require('diff');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+const wss = new WebSocket.Server({ server });
+
+// Initial SVG content
+let currentSVG = `
+<svg width="800" height="600">
+  <rect width="100%" height="100%" fill="white"/>
+</svg>
+`;
+
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 io.on('connection', (socket) => {
 	const connectTime = new Date().toISOString();
@@ -25,6 +40,39 @@ io.on('connection', (socket) => {
 		console.log('User disconnected');
 	});
 });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  // Send initial SVG to the client
+  ws.send(JSON.stringify({ type: 'initialSVG', data: currentSVG }));
+
+  ws.on('message', (message) => {
+    const data = JSON.parse(message);
+    if (data.type === 'adminUpdate') {
+      const oldSVG = currentSVG;
+      currentSVG = data.svg;
+
+      // Compute the difference between old and new SVG
+      const differences = diffChars(oldSVG, currentSVG);
+
+      // Broadcast the diff to all clients
+      wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({ type: 'svgDiff', data: differences }));
+        }
+      });
+    }
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+ws.on('close', () => {
+    console.log('Client disconnected');
+  });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
